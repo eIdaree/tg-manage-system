6407af84e64f97512a68a7daf606739cc8463d31
Eldar <eldaryergengali@gmail.com>
2024-07-25 18:32:21 +0500

    v1.1 commit

--- back/index.js
+++ back/index.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 
 const TelegramBot = require('node-telegram-bot-api');
-const webAppUrl = process.env.APPURL
+const webAppUrl = process.env.APIURL;
 const token = process.env.TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
-  const text = msg.text
+  const text = msg.text;
 
-  if(text === '/start'){
-    await bot.sendMessage(chatId,'Здравствуйте в сервис KazManageSystem', {
-        reply_markup:{
-            keybord:[
-                [{text: 'Заполни форму', web_app: {url: webAppUrl + '/form'}}]
-            ]
-        }
-    })
-    await bot.sendMessage(chatId,'Ниже появится кнопка заполни форму', {
-        reply_markup:{
-            inline_keyboard: [
-                [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-            ]
-        }
-    })
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'Welcome to KazManageSystem', {
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Enter the system', web_app: { url: webAppUrl } }]
+        ]
+      }
+    });
+
+    await bot.sendMessage(chatId, 'Ниже появится кнопка заполни форму', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Enter the system', web_app: { url: webAppUrl } }]
+        ]
+      }
+    });
   }
-
 });
